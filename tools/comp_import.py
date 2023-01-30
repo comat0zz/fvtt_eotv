1408b5097225f6ac3b47a6dde50581b863d9cd2e
Никита Белов <n.belov@sberauto.com>
2023-01-31 02:22:09 +0300

    промежуточные

--- tools/comp_import.py
+++ tools/comp_import.py
@@ -74,25 +74,67 @@ cf2.writelines(json.dumps(clusters, ensure_ascii=False, indent=4, sort_keys=True
 cf2.close()
 
 sheet = book.worksheets[2]
-princips = []
+equipments = []
 for row in range(1, 999):
 
     playbook = sheet.cell(row=row, column=1).value
     name = sheet.cell(row=row, column=2).value
+    desc = sheet.cell(row=row, column=3).value
+    in_action = sheet.cell(row=row, column=4).value
+    key = sheet.cell(row=row, column=5).value
 
     if name is None:
         break
 
-    princips.append({
+    if in_action == 1:
+        in_action = True
+    else:
+        in_action = False
+
+
+    equipments.append({
         "name": name,
         "img": "",
         "folder": None,
-        "type": "princips",
+        "type": "equipments",
         "data": {
-          "playbook": playbook
+          "playbook": playbook,
+          "key": key,
+          "in_action": in_action,
+          "description": desc
         }
     })
 
-cf3 = open('princips.json', 'w', encoding='utf-8')
-cf3.writelines(json.dumps(princips, ensure_ascii=False, indent=4, sort_keys=True))
+cf3 = open('equipments.json', 'w', encoding='utf-8')
+cf3.writelines(json.dumps(equipments, ensure_ascii=False, indent=4, sort_keys=True))
+cf3.close()
+
+
+sheet = book.worksheets[3]
+cfg = {}
+for row in range(2, 999):
+
+    playbook = sheet.cell(row=row, column=1).value
+    res_max = sheet.cell(row=row, column=2).value
+    res_tec = sheet.cell(row=row, column=3).value
+    fin_max = sheet.cell(row=row, column=4).value
+    fin_tec = sheet.cell(row=row, column=5).value
+    pr_max = sheet.cell(row=row, column=6).value
+    pr_tec = sheet.cell(row=row, column=7).value
+
+    if playbook is None:
+        break
+
+    cfg[playbook] = {
+        "res_max": res_max,
+        "res_tec": res_tec,
+        "fin_max": fin_max,
+        "fin_tec": fin_tec,
+        "pr_max": pr_max,
+        "pr_tec": pr_tec
+    }
+
+
+cf3 = open('cfg.json', 'w', encoding='utf-8')
+cf3.writelines(json.dumps(cfg, ensure_ascii=False, indent=4, sort_keys=True))
 cf3.close()
