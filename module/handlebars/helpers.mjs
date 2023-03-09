565fb75da9e5bace49a53348328739abf1eafc4e
Никита Белов <n.belov@sberauto.com>
2023-03-09 03:20:47 +0300

    промежуточный сейв

--- module/handlebars/helpers.mjs
+++ module/handlebars/helpers.mjs
@@ -99,4 +99,24 @@ export const registerHandlebarsHelpers = async function () {
     let options = args.pop();
     return args.indexOf(false) === -1 ? options.fn(this) : options.inverse(this);
   });
+
+  /**
+  * Register a debug helper for Handlebars to be able to log data or inspect data in the browser console
+  * 
+  * Usage: 
+  *   {{debug someObj.data}} => logs someObj.data to the console
+  *   {{debug someObj.data true}} => logs someObj.data to the console and stops at a debugger point
+  * 
+  * Source: https://gist.github.com/elgervb/5c38c8d70870f92ef6338a291edf88e9
+  * 
+  * @param {any} the data to log to console
+  * @param {boolean} whether or not to set a breakpoint to inspect current state in debugger
+  */
+  Handlebars.registerHelper( 'debug', function( data, breakpoint ) {
+    console.log(data);
+    if (breakpoint === true) {   
+        debugger;
+    }
+    return '';
+  });
 };
